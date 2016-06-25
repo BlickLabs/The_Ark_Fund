816cb82062f2a3bf4ec83b8fa22753bf817d7a5e
Alan Sanchez <sanchezpineda03@gmail.com>
2016-06-24 19:04:04 -0500

    Moved scripts into js directory

---
+++ js/effects.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(window).scroll(function (e) {
+    if ($('#navbar-trigger').is(':checked')) {
+      e.preventDefault();
+    } else {
+      if ($(window).scrollTop() > 100) {
+        $('#mavericks-logo').addClass('hide-text')
+      } else {
+        $('#mavericks-logo').removeClass('hide-text')
+      }
+    }
+    var footerHeight = $('footer').outerHeight(),
+      windowHeight = window.innerHeight,
+      scroll = $(window).scrollTop(),
+      footerTop = $('footer').offset().top;
+    if ((parseInt(footerTop - scroll) - (windowHeight - footerHeight) <= footerHeight) && !$('#form-section').hasClass('animate-wave')) {
+      $('#form-section').addClass('animate-wave');
+    } else if ((parseInt(footerTop - scroll) - (windowHeight - footerHeight) > footerHeight) && $('#form-section').hasClass('animate-wave')) {
+      $('#form-section').removeClass('animate-wave');
+    }
+  });
+});
